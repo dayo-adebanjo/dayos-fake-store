eac0b4440666a9ef786fc7803e9b680378f659c5
Dayo Adebanjo <aea405@nyu.edu>
2025-08-24 22:40:10 -0400

    styling main page links

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,48 @@
 import { Link } from 'react-router-dom'
+import '../style/Home.css'
 
 export default function HomePage() {
   return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Welcome</h1>
-      <p>This is a simple landing page.</p>
-      <Link to="/products">Go to Products</Link>
+    <div className="home-wrap">
+      {/* Top main grid */}
+      <section className="main">
+        <div className="main-left">
+          <h1 className="main-title">
+            WELCOME <br />
+            TO <i>FAKE.</i><br />
+          </h1>
+
+          <div className="main-cards">
+            <Link className="main-card main-card--lime" to="/products">
+              <span style={{color: "white"}}>MEN'S</span>
+              <span className="arrow">↗</span>
+            </Link>
+
+            <Link className="main-card main-card--pink" to="/about">
+              <span style={{color: "white"}}>WOMEN'S</span>
+              <span className="arrow">↗</span>
+            </Link>
+
+            <Link className="main-card main-card--orange" to="/delivery">
+              <span style={{color: "white"}}>ELECTRONICS</span>
+              <span className="arrow">↗</span>
+            </Link>
+          </div>
+        </div>
+
+        <div className="main-right">
+          <div className='right-text' style={{fontSize: 100, marginTop: "20%"}}>SALE:</div>
+          <div className='right-text' ><i>50% OFF JEWELRY</i></div>
+          <span className="arrow">↗</span>          
+        </div>
+      </section>
+
+      <div className="divider">
+        <h2>BESTSELLERS</h2>
+        <Link to="/products" className="pill-cta">
+          ALL PRODUCTS
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
